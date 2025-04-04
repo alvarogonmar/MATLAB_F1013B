7238a8372f0870e78a3c8506c72ec3fd481958a3
alvarogonmar <alvarogonmar0@gmail.com>
2025-04-04 08:08:22 -0600

    Define faces

--- Challenge/cube_patch.m
+++ Challenge/cube_patch.m
@@ -13,3 +13,14 @@ vertices = [
     1 1 1; % v7
     0 1 1; % v8
 ];
+
+% Define the faces of the cube using the vertex index
+faces = [
+  1 2 3 4; % Botton face
+  5 6 7 8; % Top face
+  1 2 6 5; % Front Face
+  2 3 7 6; % Right face
+  3 4 8 7; % Back Face
+  4 1 5 8; % Left face
+];
+
