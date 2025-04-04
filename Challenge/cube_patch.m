335e1099e27d5518b445b1ae4840f799994cc483
alvarogonmar <alvarogonmar0@gmail.com>
2025-04-04 07:47:41 -0600

    cube patch added

---
+++ Challenge/cube_patch.m
@@ -0,0 +1,15 @@
+clear;
+clc;
+close all;
+
+% Define the vertices of the cube
+vertices = [
+    0 0 0; % v1
+    1 0 0; % v2
+    1 1 0; % v3
+    0 1 0; % v4
+    0 0 1; % v5
+    1 0 1; % v6
+    1 1 1; % v7
+    0 1 1; % v8
+];
