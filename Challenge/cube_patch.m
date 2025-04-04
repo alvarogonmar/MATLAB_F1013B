0df14cddb9e759e14894f818bc07e9c838a2ba88
alvarogonmar <alvarogonmar0@gmail.com>
2025-04-04 08:08:46 -0600

    define colors

--- Challenge/cube_patch.m
+++ Challenge/cube_patch.m
@@ -24,3 +24,13 @@ faces = [
   4 1 5 8; % Left face
 ];
 
+% Define colors
+faceColors = [
+    1 0 0; % Red
+    0 1 0; % Green
+    0 0 1; % Blue
+    1 1 0; % Yellow
+    1 0 1; % Magenta
+    0 1 1 % Cyan
+];
+
