17deba5d5c2c681c0e0fa7453aad6b96b88c7b9f
alvarogonmar <alvarogonmar0@gmail.com>
2025-04-09 10:21:47 -0600

    calculating derivative

--- Derivate_of_function.m
+++ Derivate_of_function.m
@@ -9,3 +9,6 @@ f = @(x) x.^2 - 3*x - 4;
 
 % First create a symbolic variable (since we are deriving with respect x) 
 syms x;
+
+% with the internal MATLAB function we calculate the derivative with respect x
+derivative = diff(f, x);
