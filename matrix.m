10be0e8f9fdcc860a0260c3f0f1261951e38d98e
alvarogonmar <alvarogonmar0@gmail.com>
2025-04-11 08:56:44 -0600

    adding part 2 and first code finished

--- matrix.m
+++ matrix.m
@@ -12,9 +12,32 @@ f = zeros(length(y_vals), length(x_vals));
 
 % fill the matrix using nested for loops
 for i = 1:length(y_vals)
-    for j = i:length(x_vals)
+    for j = 1:length(x_vals)
         x = x_vals(j);
         y = y_vals(i);
         f(i, j) = sin(x) * cos(y); % example scalar field
     end
 end
+
+disp(['Size of f: ', num2str(size(f,1)), ' rows x', num2str(size(f,2)), ' columns'])
+
+% Create meshgrid for plotting
+[X, Y] = meshgrid(x_vals, y_vals);
+
+% Surface plot
+figure;
+surf(X, Y, f, 'EdgeColor', 'none');
+colormap parula;
+colorbar;
+title('Surface Plot of Scalar Field');
+xlabel('x');
+ylabel('y');
+zlabel('f(x, y)');
+hold on;
+
+% view settings
+view(45, 30);
+grid on;
+%% Part 2
+% Define the grid
+[x, y] = meshgrid(-5:0.1:5, -5:0.1:5);
