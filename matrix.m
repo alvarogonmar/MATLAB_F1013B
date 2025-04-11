2b28425bf0b609b7462e5e66c73749255c3ab789
alvarogonmar <alvarogonmar0@gmail.com>
2025-04-11 08:35:46 -0600

    filling matrix

--- matrix.m
+++ matrix.m
@@ -10,3 +10,11 @@ y_vals = linspace(-5, 5, 100);
 % Preallocate the scalar field matrix
 f = zeros(length(y_vals), length(x_vals));
 
+% fill the matrix using nested for loops
+for i = 1:length(y_vals)
+    for j = i:length(x_vals)
+        x = x_vals(j);
+        y = y_vals(i);
+        f(i, j) = sin(x) * cos(y); % example scalar field
+    end
+end
