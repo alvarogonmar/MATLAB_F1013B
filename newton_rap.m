a5d9c8067676fe349790c45137778b9211ef74dc
alvarogonmar <alvarogonmar0@gmail.com>
2025-04-11 07:43:31 -0600

    adding comments and result

--- newton_rap.m
+++ newton_rap.m
@@ -1,17 +1,19 @@
 function [r, count] = newton_rap(f, x0, tol)
 
 syms x;
-g = matlabFunction(diff(f, x));
+g = matlabFunction(diff(f, x)); % Derivada de f
 counter = 0;
 
 while True
     counter = counter + 1;
-    x_new = x0 - (f(x0)/g(x0));
+    x_new = x0 - (f(x0)/g(x0)); % Fórmula de Newton-Raphson
     error = abs((x_new - x0) / x_new); % Error relativo
     if error < tol
         break;
     end
+    x0 = x_new; % Actualizar x0 para la próxima iteración
 
 end
 
-count = counter;
+r = x_new; % Resultado final
+count = counter; % Número de iteraciones
