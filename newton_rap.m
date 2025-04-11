fd62b5a3b350712e7896f8e5313bfd2a0f359a85
alvarogonmar <alvarogonmar0@gmail.com>
2025-04-11 07:48:41 -0600

    function finished

--- newton_rap.m
+++ newton_rap.m
@@ -6,8 +6,15 @@ counter = 0;
 
 while True
     counter = counter + 1;
+    % Verificar si f(x0) es igual a 0
+    if f(x0) == 0
+        r = x0; % Si f(x0) = 0, la raíz es x0
+        count = counter;
+        return;
+    end
     x_new = x0 - (f(x0)/g(x0)); % Fórmula de Newton-Raphson
     error = abs((x_new - x0) / x_new); % Error relativo
+    x_new = x0;
     if error < tol
         break;
     end
@@ -15,5 +22,5 @@ while True
 
 end
 
-r = x_new; % Resultado final
+r = x0; % Resultado final
 count = counter; % Número de iteraciones
