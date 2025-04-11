d37f263ccee1549a6789896befe195aada4f2040
alvarogonmar <alvarogonmar0@gmail.com>
2025-04-11 08:00:39 -0600

    function fixed and finished

--- newton_rap.m
+++ newton_rap.m
@@ -1,26 +1,20 @@
-function [r, count] = newton_rap(f, x0, tol)
+function [r, cont] = newton_rap(f, x0, tol)
 
 syms x;
 g = matlabFunction(diff(f, x)); % Derivada de f
 counter = 0;
 
-while True
+while true
     counter = counter + 1;
-    % Verificar si f(x0) es igual a 0
-    if f(x0) == 0
-        r = x0; % Si f(x0) = 0, la raíz es x0
-        count = counter;
-        return;
-    end
-    x_new = x0 - (f(x0)/g(x0)); % Fórmula de Newton-Raphson
+    x_new = x0 - f(x0)/g(x0); % Fórmula de Newton-Raphson
     error = abs((x_new - x0) / x_new); % Error relativo
-    x_new = x0;
+    x0 = x_new;
     if error < tol
         break;
     end
-    x0 = x_new; % Actualizar x0 para la próxima iteración
-
 end
 
 r = x0; % Resultado final
-count = counter; % Número de iteraciones
+cont = counter; % Número de iteraciones
+
+end
